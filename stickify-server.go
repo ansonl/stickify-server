9aaaaf2b99ee81c4090ee7cecd6742f55a1a9b6a
ansonl <ansonl@users.noreply.github.com>
2016-08-24 02:23:00 +0000

    remove print form data from updateHandler

--- stickify-server.go
+++ stickify-server.go
@@ -464,7 +464,6 @@ func updateHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	r.ParseForm()
-	fmt.Println(r.Form)
 
 	var user string
 	var passcode string
